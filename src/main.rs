55e766acd8876d6d43908179986aa41ff19291ba
Johann M. Barnard <johann.b@telus.net>
2024-05-12 12:04:35 -0600

    Add support for booting via BIOS

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,26 @@
 
 use core::panic::PanicInfo;
 
-/// Called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+static HELLO: &[u8] = b"Hello world!";
+
+/// Kernel entry point.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // cyan coloured
+        }
+    }
+
+    #[allow(clippy::empty_loop)]
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+/// Called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
